304da23739acab7930d515d24e9b4f54791cd02d
yorgis711 <giorgos@yorgis.net>
2025-08-29 18:31:27 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -4,16 +4,15 @@ import { DurableObject } from "cloudflare:workers";
 export default {
   async fetch(request, env) {
     
-    // A stub is a client Object used to send messages to the Durable Object.
     let stub = env.ClassDO.getByName(name);
 
-    // Send a request to the Durable Object using RPC methods, then await its response.
+    
     
     return new Response();
   },
 };
 
 // Durable Object
-export class Counter extends DurableObject {
+export class class-manager-classdo extends DurableObject {
   
       }
